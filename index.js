d50c4d94fd7ee847e8d8632bea31e9f7df0f46ea
Bogdan Stefanescu <bogdan@vintners.co>
2022-11-07 22:21:40 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+"use strict";
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AlignedImage = exports.getAlignmentClass = exports.setAlignmentClassMapper = exports.getDefaultAlignmentClass = void 0;
+const extension_image_1 = __importDefault(require("@tiptap/extension-image"));
+function getDefaultAlignmentClass(align) {
+    if (!align)
+        return null;
+    switch (align) {
+        case 'left': return 'is-align-left';
+        case 'right': return 'is-align-right';
+        case 'center': return 'is-align-center';
+        default: return null;
+    }
+}
+exports.getDefaultAlignmentClass = getDefaultAlignmentClass;
+let mapperFn = getDefaultAlignmentClass;
+function setAlignmentClassMapper(mapper) {
+    mapperFn = mapper;
+}
+exports.setAlignmentClassMapper = setAlignmentClassMapper;
+function getAlignmentClass(align) {
+    return mapperFn(align);
+}
+exports.getAlignmentClass = getAlignmentClass;
+const AlignedImage = extension_image_1.default.extend({
+    name: 'alignedImage',
+    defaultOptions: {
+        align: ['center', 'left', 'right', 'none']
+    },
+    addAttributes() {
+        var _a;
+        return Object.assign(Object.assign({}, (_a = extension_image_1.default.config.addAttributes) === null || _a === void 0 ? void 0 : _a.call(this)), { class: {
+                default: null,
+                rendered: true
+            } });
+    },
+    addCommands() {
+        return {
+            setAlignedImage: options => ({ commands }) => {
+                const { align } = options, attrs = __rest(options, ["align"]);
+                return commands.insertContent({
+                    type: this.name,
+                    attrs: Object.assign({ class: getAlignmentClass(align) }, options)
+                });
+            },
+        };
+    }
+});
+exports.AlignedImage = AlignedImage;
---
+++ index.ts
@@ -0,0 +1,63 @@
+import Image from '@tiptap/extension-image';
+
+declare module '@tiptap/core' {
+    interface Commands<ReturnType> {
+        alignedImage: {
+            /**
+             * Add an image
+             */
+            setAlignedImage: (options: { src: string, alt?: string, title?: string, align?: 'left' | 'right' | 'center' | 'none' }) => ReturnType,
+        }
+    }
+}
+
+export function getDefaultAlignmentClass(align?: 'left' | 'right' | 'center' | 'none' | null) {
+    if (!align) return null;
+    switch (align) {
+        case 'left': return 'is-align-left';
+        case 'right': return 'is-align-right';
+        case 'center': return 'is-align-center';
+        default: return null;
+    }
+}
+export type MapperFn = (align?: 'left' | 'right' | 'center' | 'none' | null) => string | null;
+
+let mapperFn: MapperFn = getDefaultAlignmentClass;
+
+export function setAlignmentClassMapper(mapper: MapperFn) {
+    mapperFn = mapper;
+}
+
+export function getAlignmentClass(align?: 'left' | 'right' | 'center' | 'none' | null) {
+    return mapperFn(align);
+}
+
+const AlignedImage = Image.extend({
+    name: 'alignedImage',
+    defaultOptions: {
+        align: ['center', 'left', 'right', 'none']
+    },
+    addAttributes() {
+        return {
+            ...Image.config.addAttributes?.call(this),
+            class: {
+                default: null,
+                rendered: true
+            }
+        }
+    },
+    addCommands() {
+        return {
+            setAlignedImage: options => ({ commands }) => {
+                const { align, ...attrs } = options;
+
+                return commands.insertContent({
+                    type: this.name,
+                    attrs: { class: getAlignmentClass(align), ...options }
+                })
+            },
+        }
+    }
+});
+
+export { AlignedImage };
